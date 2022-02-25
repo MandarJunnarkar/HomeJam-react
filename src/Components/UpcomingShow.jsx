64ac939fe70ffef9bff256f513de8d9ba34e5e4e
Mandar Junnarkar <mandarjunnarkar222@gmail.com>
2022-02-25 18:04:58 +0530

    padding fix

--- src/Components/UpcomingShow.jsx
+++ src/Components/UpcomingShow.jsx
@@ -3,7 +3,7 @@ import ProfileCard from "./ProfileCard";
 
 const UpcomingShow = () => {
   return (
-    <div className="flex flex-col pt-10 lg:pt-36 bg-blue-black px-2 lg:px-20 h-full w-full">
+    <div className="flex flex-col pt-16 lg:pt-36 bg-blue-black px-2 lg:px-20 h-full w-full">
       <div className="text-white flex justify-between">
         <h1 className="font-libre-baskerville text-2xl">Upcoming Shows</h1>
         <a href="/" className="text-yellow-500 text-sm">View all</a>
